c099aa1fe2ea8f47d2d078adcd47ad21c578f29f
Panique <panique@web.de>
2013-04-27 01:18:27 +0200

    cleaning the script for proper new version

--- config/messages.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/*
- * Configuration file for: Messages
- * This is the place where you store your messages and texts.
- * It's a much better way to keep this stuff in a config file that writing it directly into your code.
- */
