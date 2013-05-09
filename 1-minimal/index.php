a67c5b246c107193ff76fe3e52e9dff1c41410c4
Panique <panique@web.de>
2013-05-10 01:16:35 +0200

    typo in comment

--- 1-minimal/index.php
+++ 1-minimal/index.php
@@ -26,7 +26,7 @@ require_once("config/db.php");
 // load the login class
 require_once("classes/Login.php");
 
-// create a login object. when this object is created, it will do all login/logout stuff automaticly
+// create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
 $login = new Login();
 
