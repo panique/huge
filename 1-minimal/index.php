094046e1e7bfe3ad208840710561615370aa7b2e
Panique <panique@web.de>
2013-05-12 02:58:00 +0200

    beauty corrections

--- 1-minimal/index.php
+++ 1-minimal/index.php
@@ -31,8 +31,7 @@ require_once("classes/Login.php");
 $login = new Login();
 
 // ... ask if we are logged in here:
-if ($login->isUserLoggedIn() == true) {
-    
+if ($login->isUserLoggedIn() == true) {    
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
     include("views/logged_in.php");
