a2b47647585da027841b1c69809c8a0f3add9229
Panique <panique@web.de>
2013-05-18 17:05:53 +0200

    new SQL statements (email field cannot be NULL anymore)

--- 1-minimal/_install/users.sql
+++ 1-minimal/_install/users.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 09. Mai 2013 um 22:46
+-- Erstellungszeit: 18. Mai 2013 um 15:54
 -- Server Version: 5.5.29-log
 -- PHP-Version: 5.3.21
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name',
   `user_password_hash` char(118) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
-  `user_email` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'user''s email',
+  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_name` (`user_name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data' AUTO_INCREMENT=1 ;
