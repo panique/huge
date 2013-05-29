27e116249915e6e99e20bc89b03a1db0e51e60a9
Panique <panique@web.de>
2013-05-29 15:56:07 +0200

    fix: new 6 char password minimum limit [issue #54 + #64]

--- 1-minimal/views/register.php
+++ 1-minimal/views/register.php
@@ -28,13 +28,13 @@ if ($registration->messages) {
     
     <!-- the email input field uses a HTML5 pattern check (3-64 characters) and a HTML5 email type check -->
     <label for="login_input_email">User's email</label>    
-    <input id="login_input_email" class="login_input" type="email" pattern="(.*){6,64}" name="user_email" required />        
+    <input id="login_input_email" class="login_input" type="email" name="user_email" required />        
     
     <label for="login_input_password_new">Password</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />  
+    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern="(.*){6,64}"  oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />  
     
     <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />        
+    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern="(.*){6,64}" oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />        
     <input type="submit"  name="register" value="Register" />
     
 </form>
