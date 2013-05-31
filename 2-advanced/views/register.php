3fac2635538333b2972f75886b9ea51273b5e90b
Panique <panique@web.de>
2013-06-01 01:51:02 +0200

    removed password max. limit

--- 2-advanced/views/register.php
+++ 2-advanced/views/register.php
@@ -26,15 +26,15 @@ if ($registration->messages) {
     <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
     <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
     
-    <!-- the email input field uses a HTML5 pattern check (3-64 characters) and a HTML5 email type check -->
+    <!-- the email input field uses a HTML5 email type check -->
     <label for="login_input_email">User's email</label>    
     <input id="login_input_email" class="login_input" type="email" name="user_email" required />
     
     <label for="login_input_password_new">Password</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern="(.*){6,64}" oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />  
+    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />  
     
     <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern="(.*){6,64}" oninvalid="setCustomValidity('Minimum length is 6 characters')" required autocomplete="off" />        
+    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />        
     <input type="submit"  name="register" value="Register" />
     
 </form>
