5f0ed9ace88239c1e72943923320379d85b5c15b
Panique <panique@web.de>
2013-06-28 01:58:57 +0200

    auto-check for minimum php version

--- 1-minimal/register.php
+++ 1-minimal/register.php
@@ -21,6 +21,11 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
+// checking for minimum PHP version
+if (version_compare(PHP_VERSION, '5.3.7', '<') ) {    
+  exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");  
+}
+
 // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
 // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
 require_once("libraries/password_compatibility_library.php");
