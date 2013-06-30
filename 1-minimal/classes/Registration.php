9bbf08c0957eb7918dc0d0c3c17de305f0f81d21
nyamsprod <nyamsprod@gmail.com>
2013-06-30 11:56:11 +0200

    Update Registration.php
    Updated Error message to better handle internationalisation move DB connection into the constructor method

--- 1-minimal/classes/Registration.php
+++ 1-minimal/classes/Registration.php
@@ -9,31 +9,24 @@
 */
 class Registration 
 {
-    private $db_connection; // database connection   
+    private $conn; // database connection   
     private $is_registration_ok = false;
     private $errors = array();  // collection of error messages
-    private $errorMessages = array(
-        'user_name' => array(
-            'missing' => 'Username is missing',
-            'invalid' =>'Username does not fit the name sheme: only a-Z and numbers are allowed, 2 to 64 characters',
-        ),
-        'user_email' => array(
-            'missing' => 'Email missing',
-            'invalid' =>'Invalid Email',
-        ),
-        'user_password_new' => array(
-            'missing' => 'Password missing',
-            'invalid' =>'Password has a minimum length of 6 characters',
-        ),
-        'user_password_repeat' => array(
-            'missing' => 'Password missing',
-            'invalid' =>'Password has a minimum length of 6 characters',
-        ),
-    );
+
+    const DATA_MISSING = 1;
+    const DATA_INVALID = 2;
+    const DATA_MISMATCH = 3;
+    const REGISTRATION_FAILED = 1;
+    const USER_EXISTS = 1;
  
     public function __construct() 
     {
-        $this->is_registration_ok = $this->registerNewUser();
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($this->conn->connect_errno || ! $this->conn->set_charset(DB_CHARSET)) {
+            die("Sorry, no database connection.");
+        }
+
+        $this->$is_registration_ok = $this->registerNewUser();
     }
 
     public function getErrors($name = null)
@@ -62,7 +55,7 @@ class Registration
     {
         $this->errors = array();
         $this->messages = array();
-        if (! filter_has_var(INPUT_POST, 'register')) {
+        if (filter_has_var(INPUT_POST, 'register')) {
             return false;
         }
 
@@ -75,55 +68,47 @@ class Registration
         );
         $params = filter_input_array(INPUT_POST, $arguments);
         if (! $params) {
-            $this->errors['submission'] = 'Empty Submission';
+            $this->errors['submission'] = self::DATA_MISSING;
             return false;
         }
 
         foreach (array_keys($arguments) as $keys) {
             $value = $params[$keys];
             if (is_null($value)) {
-                $this->errors[$keys] = $this->errorMessages[$keys]['missing'];
+                $this->errors[$keys] = self::DATA_MISSING;
             } elseif (! $value) {
-                $this->errors[$keys] = $this->errorMessages[$keys]['invalid'];
+                $this->errors[$keys] = self::DATA_INVALID;
             }
         }
         if (empty($this->errors) && ($params['user_password_new'] != $params['user_password_repeat'])) {
-            $this->errors['user_password'] = "Password and password repeat are not the same";
+            $this->errors['user_password'] = self::DATA_MISMATCH;
         }
         if (count($this->errors)) {
             return false;
         }
 
-        //2 - DB Connection
-        $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if ($this->db_connection->connect_errno || ! $this->db_connection->set_charset(DB_CHARSET)) {
-            $this->errors['connection'] = "Sorry, no database connection.";
-            return false;
-        }
-
-
-        //data prepare and sanitaze for db inclusion
+        //2 - data prepared and sanitized for db inclusion
         $params['user_password'] = password_hash($params['user_password_new'], PASSWORD_DEFAULT);
         unset($params['user_password_new'], $params['user_password_repeat']);
-        $params = array_map(array($this->db_connection, 'real_escape_string'), $params);
+        $params = array_map(array($this->conn, 'real_escape_string'), $params);
 
-        //3-  check if user already exists
-        $res = $this->db_connection->query(
+        //3 - check if user already in the table
+        $res = $this->conn->query(
             "SELECT * FROM users WHERE user_name = '{$params['user_name']}' OR user_email = '{$params['user_email']}'"
         );
 
         if ($res->num_rows > 0) {
-            $this->errors['uniqueness'] = "Sorry, the user name OR the email is already taken.<br/>Please choose another one.";
+            $this->errors['uniqueness'] = self::USER_EXISTS
             return false;
         }
 
-        // write new users data into database
-        $res = $this->db_connection->query(
+        //4 - write new users data into database
+        $res = $this->conn->query(
             "INSERT INTO users (".implode(',', array_keys($params)).") VALUES ('."implode("','", $params)".')"
         );
 
         if (! $res) {
-            $this->errors['registration'] = "Sorry, your registration failed. Please go back and try again.";
+            $this->errors['registration'] = self::REGISTRATION_FAILED;
             return false;
         }
 
