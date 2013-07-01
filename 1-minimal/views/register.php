51c8c7bf26fb87c47ec0da58177a1d6ad3c64a64
nyamsprod <nyamsprod@gmail.com>
2013-07-01 10:38:38 +0200

    Update register.php
    using Auth::$regexp to normalize pattern info

--- 1-minimal/views/register.php
+++ 1-minimal/views/register.php
@@ -24,17 +24,17 @@ if ($registration->messages) {
     
     <!-- the user name input field uses a HTML5 pattern check -->
     <label for="login_input_username">Username (only letters and numbers, 2 to 64 characters)</label>
-    <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+    <input id="login_input_username" class="login_input" type="text" pattern="<?php echo Auth::$regexp['user_name']; ?>" name="user_name" required />
     
     <!-- the email input field uses a HTML5 email type check -->
     <label for="login_input_email">User's email</label>    
     <input id="login_input_email" class="login_input" type="email" name="user_email" required />        
     
     <label for="login_input_password_new">Password (min. 6 characters)</label>
-    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />  
+    <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern="<?php echo Auth::$regexp['user_password']; ?>" required autocomplete="off" />  
     
     <label for="login_input_password_repeat">Repeat password</label>
-    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />        
+    <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".<?php echo Auth::$regexp['user_password']; ?>" required autocomplete="off" />        
     <input type="submit"  name="register" value="Register" />
     
 </form>
