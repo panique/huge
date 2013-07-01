c5332c480a640353f56accaec232f5533726a8ae
nyamsprod <nyamsprod@gmail.com>
2013-07-01 10:46:00 +0200

    Update index.php

--- 1-minimal/index.php
+++ 1-minimal/index.php
@@ -23,26 +23,27 @@
 
 // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
 // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-require_once("libraries/password_compatibility_library.php");
+require "libraries/password_compatibility_library.php";
 
 // include the configs / constants for the database connection
-require_once("config/db.php");
+require "config/db.php";
+
+// load the Auth class
+require "classes/Auth.php";
 
 // load the login class
-require_once("classes/Login.php");
+require "classes/Login.php";
+
+// by default the user is not logged in. you can do whatever you want here.
+// for demonstration purposes, we simply show the "you are not logged in" view.
+$view = "not_logged_in";
 
 // create a login object. when this object is created, it will do all login/logout stuff automatically
 // so this single line handles the entire login process. in consequence, you can simply ...
 $login = new Login();
-
-// ... ask if we are logged in here:
-if ($login->isUserLoggedIn() == true) {    
+if ($login->isUserLoggedIn()) {    
     // the user is logged in. you can do whatever you want here.
     // for demonstration purposes, we simply show the "you are logged in" view.
-    include("views/logged_in.php");
-    
-} else {
-    // the user is not logged in. you can do whatever you want here.
-    // for demonstration purposes, we simply show the "you are not logged in" view.
-    include("views/not_logged_in.php");
+    $view = "logged_in";
 }
+include 'views/'.$view.'.php';
