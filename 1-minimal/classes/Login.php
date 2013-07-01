9216378885847ece7f23e3e96925edf8de7639dd
nyamsprod <nyamsprod@gmail.com>
2013-07-01 08:37:44 +0200

    Login/php extends Auth.php
    Login is rewrite to extends Auth

--- 1-minimal/classes/Login.php
+++ 1-minimal/classes/Login.php
@@ -1,150 +1,143 @@
 <?php
 
 /**
- * class Login
- * handles the user login/logout/session
- * 
- * @author Panique <panique@web.de>
- * @version 1.2
- */
-class Login {
+* class Login
+* handles the user login/logout/session
+*
+* @author Panique <panique@web.de>
+* @version 1.2
+*/
+class Login extends Auth
+{
+   private $is_logged_in = false; // status of login
+   private $is_logged_out = false;
 
-    private     $db_connection              = null;                     // database connection
-    
-    private     $user_name                  = "";                       // user's name
-    private     $user_email                 = "";                       // user's email
-    private     $user_password_hash         = "";                       // user's hashed and salted password
-    private     $user_is_logged_in          = false;                    // status of login
+   /**
+    * the function "__construct()" automatically starts whenever an object of this class is created,
+    * you know, when you do "$login = new Login();"
+    */    
+   public function __construct() {
+       
+       parent::__construct();
 
-    public      $errors                     = array();                  // collection of error messages
-    public      $messages                   = array();                  // collection of success / neutral messages
-    
-    
-    /**
-     * the function "__construct()" automatically starts whenever an object of this class is created,
-     * you know, when you do "$login = new Login();"
-     */    
-    public function __construct() {
-        
-        // create/read session
-        session_start();                                        
+       // create/read session
+       if (empty(session_id())) {
+           session_start();
+       }                          
 
-        // check the possible login actions:
-        // 1. logout (happen when user clicks logout button)
-        // 2. login via session data (happens each time user opens a page on your php project AFTER he has sucessfully logged in via the login form)
-        // 3. login via post data, which means simply logging in via the login form. after the user has submit his login/password successfully, his
-        //    logged-in-status is written into his session data on the server. this is the typical behaviour of common login scripts.
-        
-        // if user tried to log out
-        if (isset($_GET["logout"])) {
+       $this->is_logged_out = false;
+       $this->is_logged_in = false;
+       if (isset($_GET["logout"])) {
+           $this->is_logged_out = $this->doLogout();
+           if ($this->is_logged_out) {
+               $this->is_logged_in = false;
+           }
+           return;
+       }
 
-            $this->doLogout();
-                    
-        } 
-        // if user has an active session on the server
-        elseif (!empty($_SESSION['user_name']) && ($_SESSION['user_logged_in'] == 1)) {
+       // if user has an active session on the server
+       if (isset($_SESSION['user_name'], $_SESSION['user_logged_in']) && 1 == $_SESSION['user_logged_in']) {
+           $this->is_logged_in = $this->loginWithSessionData();                
+           return;
 
-            $this->loginWithSessionData();                
+       }
 
-        // if user just submitted a login form
-        } elseif (isset($_POST["login"])) {
+       // if user just submitted a login form
+       if (isset($_POST["login"])) {
+           $this->is_logged_in = $this->loginWithPostData();
+       }        
+   }    
+   
+   private function loginWithSessionData()
+   {
+       // set logged in status to true, because we just checked for this:
+       // !empty($_SESSION['user_name']) && ($_SESSION['user_logged_in'] == 1)
+       // when we called this method (in the constructor)
+       $login = filter_var($_SESSION['user_name'], FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/^[a-z0-9]{2,64}$/i')));
+       if (! $login) {
+            $this->errors['session'] = self::DATA_INVALID;
+            return false;
+       }
 
-                $this->loginWithPostData();
-                
-        }        
-    }    
-    
+       //2 - DB Connection
+       $res = $this->conn->query(
+           "SELECT * FROM users WHERE user_name = '".$this->conn->real_escape_string($login)."'"
+       );
+       if ($res->num_rows != 1) {
+           $this->errors['user'] = self::USER_UNKNOWN;
+           return false;
+       }
 
-    private function loginWithSessionData() {
-        
-        // set logged in status to true, because we just checked for this:
-        // !empty($_SESSION['user_name']) && ($_SESSION['user_logged_in'] == 1)
-        // when we called this method (in the constructor)
-        $this->user_is_logged_in = true;
-        
-    }
-    
+       $user = $res->fetch_assoc();
+       foreach ($user as $key => $value) {
+           $_SESSION[$key] = $value;
+       }
+       $_SESSION['user_logged_in'] = 1;
+       return true;  
+   }
+   
+   private function loginWithPostData()
+   {
+       if (! filter_has_var(INPUT_POST, 'user_name') || ! filter_has_var(INPUT_POST, 'user_password') ) {
+            $this->errors['submission'] = self::DATA_MISSING;
+            return false;
+       }
 
-    private function loginWithPostData() {
-        
-        // if POST data (from login form) contains non-empty user_name and non-empty user_password
-        if (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
-            
-            // create a database connection, using the constants from config/db.php (which we loaded in index.php)
-            $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-            
-            // if no connection errors (= working database connection)
-            if (!$this->db_connection->connect_errno) {
-                
-                // escape the POST stuff
-                $this->user_name = $this->db_connection->real_escape_string($_POST['user_name']);            
-                // database query, getting all the info of the selected user
-                $checklogin = $this->db_connection->query("SELECT user_name, user_email, user_password_hash FROM users WHERE user_name = '".$this->user_name."';");
+       $params = filter_input_array(
+           INPUT_POST,
+           array(
+               'user_name' => array('filter' => FILTER_VALIDATE_REGEXP, 'options' => array('regexp' => '/^[a-z0-9]{2,64}$/i')),
+               'user_password' => array('filter' => FILTER_VALIDATE_REGEXP, 'options' => array('regexp' => '/^.{6,}$/')),
+           )
+       )
 
-                // if this user exists
-                if ($checklogin->num_rows == 1) {
+       if (! isset($params['user_name'], $params['user_password'])) {
+            $this->errors['validation'] = self::DATA_INVALID;
+            return false;
+       }
 
-                    // get result row (as an object)
-                    $result_row = $checklogin->fetch_object();
+       $params['user_name'] = $this->conn->real_escape_string($params['user_name']);
+       $res = $this->conn->query("SELECT * FROM users WHERE user_name = '{$params['user_name']}'");
+       if ($res->num_rows != 1) {
+           $this->errors['user'] = self::USER_UNKNOWN;
+           return false;
+       }
 
-                    // using PHP 5.5's password_verify() function to check if the provided passwords fits to the hash of that user's password
-                    if (password_verify($_POST['user_password'], $result_row->user_password_hash)) {
+       $user = $res->fetch_assoc();
+       if (! password_verify($param['user_password'], $user['user_password_hash'])) {
+            $this->errors['password'] = self::DATA_INVALID;
+            return false;
+       }
 
-                        // write user data into PHP SESSION [a file on your server]
-                        $_SESSION['user_name'] = $result_row->user_name;
-                        $_SESSION['user_email'] = $result_row->user_email;
-                        $_SESSION['user_logged_in'] = 1;
+       foreach ($user as $key => $value) {
+           $_SESSION[$key] = $value;
+       }
+       $_SESSION['user_logged_in'] = 1;
+       return true;      
+   }
+   
+   /**
+    * perform the logout
+    */
+   public function doLogout()
+   {
+       $_SESSION = array();
+       session_destroy();  
+       return true;
+   }
+   
+   /**
+    * simply return the current state of the user's login
+    * @return boolean user's login status
+    */
+   public function isUserLoggedIn()
+   {
+       return $this->is_logged_in;
+   }
 
-                        // set the login status to true
-                        $this->user_is_logged_in = true; 
-
-                    } else {
-
-                        $this->errors[] = "Wrong password. Try again.";
-
-                    }                
-
-                } else {
-
-                    $this->errors[] = "This user does not exist.";
-                }
-                
-            } else {
-                
-                $this->errors[] = "Database connection problem.";
-            }
-            
-        } elseif (empty($_POST['user_name'])) {
-
-            $this->errors[] = "Username field was empty.";
-
-        } elseif (empty($_POST['user_password'])) {
-
-            $this->errors[] = "Password field was empty.";
-        }           
-        
-    }
-    
-    /**
-     * perform the logout
-     */
-    public function doLogout() {
-            
-            $_SESSION = array();
-            session_destroy();
-            $this->user_is_logged_in = false;
-            $this->messages[] = "You have been logged out.";     
-            
-    }
-    
-    /**
-     * simply return the current state of the user's login
-     * @return boolean user's login status
-     */
-    public function isUserLoggedIn() {
-        
-        return $this->user_is_logged_in;
-        
-    }
+   public function isUserLogOut()
+   {
+       return $this->is_logged_out;
+   }
 
 }
