806cc8f9b42722fb6f5143429513f0274825422a
nyamsprod <nyamsprod@gmail.com>
2013-07-01 12:34:48 +0200

    added DB_CHARSET constant and using PHP 5.3+ const keyword

--- 1-minimal/config/db.php
+++ 1-minimal/config/db.php
@@ -10,14 +10,17 @@
 
 
 /** database host, usually it's "127.0.0.1" or "localhost", some servers also need port info, like "127.0.0.1:8080" */
-define("DB_HOST", "127.0.0.1");
+const DB_HOST = '127.0.0.1';
 
 /** name of the database. please note: database and database table are not the same thing! */
-define("DB_NAME", "login");
+const DB_NAME = 'login';
 
 /** user for your database. the user needs to have rights for SELECT, UPDATE, DELETE and INSERT.
 /** By the way, it's bad style to use "root", but for development it will work */
-define("DB_USER", "root");
+const DB_USER = 'root';
 
 /** The password of the above user */
-define("DB_PASS", "mysql");
+const DB_PASS = 'mysql';
+
+/** Required to better sanitize your data before DB insertion */
+const DB_CHARSET = 'utf8';
