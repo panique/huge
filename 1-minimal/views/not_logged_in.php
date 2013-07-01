1400bf1e40143bb35808313f800e3b10475d2c74
nyamsprod <nyamsprod@gmail.com>
2013-07-01 10:39:41 +0200

    Update not_logged_in.php

--- 1-minimal/views/not_logged_in.php
+++ 1-minimal/views/not_logged_in.php
@@ -23,10 +23,10 @@ if ($login->messages) {
 <!-- login form box -->
 <form method="post" action="index.php" name="loginform">
     <label for="login_input_username">Username</label>
-    <input id="login_input_username" class="login_input" type="text" name="user_name" required />
+    <input id="login_input_username" class="login_input" type="text" name="user_name" required pattern="<?php echo Auth::$regexp['user_name']; ?>">
     <label for="login_input_password">Password</label>
-    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required />
-    <input type="submit"  name="login" value="Log in" />
+    <input id="login_input_password" class="login_input" type="password" name="user_password" autocomplete="off" required pattern="<?php echo Auth::$regexp['user_password']; ?>">
+    <input type="submit" name="login" value="Log in">
 </form>
 
 <a href="register.php">Register new account</a>
