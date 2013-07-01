04b8800337384123455bd1b6956a46428aaf4915
nyamsprod <nyamsprod@gmail.com>
2013-07-01 08:46:02 +0200

    Added function to validate submitted data

--- 1-minimal/classes/Auth.php
+++ 1-minimal/classes/Auth.php
@@ -11,6 +11,10 @@ class Auth
 {
    private $conn; // database connection  
    private $errors = array();  // collection of error messages
+   private $regexp = array(
+      'user_name' => '^[a-zA-Z0-9]{2,64}$',
+      'user_password' => '^.{6,}$'
+   );
 
    const DATA_MISSING = 1;
    const DATA_INVALID = 2;
@@ -18,7 +22,7 @@ class Auth
    const REGISTRATION_FAILED = 1;
    const USER_EXISTS = 1;
    const USER_UNKNOWN = 2;
-
+   
    public function __construct()
    {
        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
@@ -36,6 +40,62 @@ class Auth
            return $this->errors[$name];
        }
        return null;
+   }
+
+   protected function isValidEmail($str = null)
+   {
+      if (is_null($str)) {
+         return null;
+      }
+      $str = filter_var($str, FILTER_VALIDATE_EMAIL);
+      if (! $str || 64 > strlen($str)) {
+         return false;
+      }
+      
+      return $str;
+   }
+
+   protected function isValidPassword($str = null)
+   {
+      if (is_null($str)) {
+         return null;
+      }
+      $str = filter_var(
+         $str,
+         FILTER_VALIDATE_REGEXP,
+         array(
+            'options' => array(
+               'regexp' => '/'.$this->regexp['user_password'].'/'
+            )
+         )
+      );
+      if (! $str) {
+         return false;
+      }
+      
+      return $str;
+   }
+
+   protected function isValidUserName($str = null)
+   {
+      if (is_null($str)) {
+         return null;
+      }
+      $str = filter_var(
+         $str,
+         FILTER_VALIDATE_REGEXP,
+         array(
+            'options' => array(
+               'regexp' => '/'.$this->regexp['user_name'].'/'
+            )
+         )
+      );
+      if (! $str) {
+         return false;
+      }
+      
+      return $str;
+      
    }
 
 }
