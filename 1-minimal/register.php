b858f08f8f0e27fd07100a60241d00c9ab484ce6
nyamsprod <nyamsprod@gmail.com>
2013-07-01 10:47:19 +0200

    Update register.php

--- 1-minimal/register.php
+++ 1-minimal/register.php
@@ -23,17 +23,20 @@
 
 // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
 // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-require_once("libraries/password_compatibility_library.php");
+require "libraries/password_compatibility_library.php";
 
 // include the configs / constants for the database connection
-require_once("config/db.php");
+require "config/db.php";
+
+// load the Auth class
+require "classes/Auth.php";
 
 // load the registration class
-require_once("classes/Registration.php");
+require "classes/Registration.php";
 
 // create the registration object. when this object is created, it will do all registration stuff automaticly
 // so this single line handles the entire registration process.
 $registration = new Registration();
 
 // showing the register view (with the registration form, and messages/errors)
-include("views/register.php");
+include "views/register.php";
