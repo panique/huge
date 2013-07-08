277b5257d767787251a565a464a6fa6070685ef8
Panique <panique@web.de>
2013-07-08 20:35:46 +0200

    added email sender name support

--- 4-full-mvc-framework/config/config.php
+++ 4-full-mvc-framework/config/config.php
@@ -96,11 +96,14 @@ define("EMAIL_SMTP_PASSWORD", 'yourpassword');
 /**
  * Configuration for: Email content data
  * 
+ * php-login uses the PHPMailer library, please have a look here if you want to add more
+ * config stuff: @see https://github.com/PHPMailer/PHPMailer
+ * 
  * As email sending within your project needs some setting, you can do this here:
  * 
  * Absolute URL to password reset action, necessary for email password reset links
  * define("EMAIL_PASSWORDRESET_URL", "http://127.0.0.1/php-login/4-full-mvc-framework/login/passwordReset"); 
- * define("EMAIL_PASSWORDRESET_FROM", "noreply@example.com");
+ * define("EMAIL_PASSWORDRESET_FROM_EMAIL", "noreply@example.com");
  * define("EMAIL_PASSWORDRESET_SUBJECT", "Password reset for PROJECT XY");
  * define("EMAIL_PASSWORDRESET_CONTENT", "Please click on this link to reset your password:");
  * 
@@ -111,11 +114,13 @@ define("EMAIL_SMTP_PASSWORD", 'yourpassword');
  * define("EMAIL_VERIFICATION_CONTENT", "Please click on this link to activate your account:");
  */
 define("EMAIL_PASSWORDRESET_URL", "http://127.0.0.1/php-login/4-full-mvc-framework/login/verifypasswordrequest");
-define("EMAIL_PASSWORDRESET_FROM", "noreply@example.com");
+define("EMAIL_PASSWORDRESET_FROM_EMAIL", "noreply@example.com");
+define("EMAIL_PASSWORDRESET_FROM_NAME", "My Project");
 define("EMAIL_PASSWORDRESET_SUBJECT", "Password reset for PROJECT XY");
 define("EMAIL_PASSWORDRESET_CONTENT", "Please click on this link to reset your password: ");
 
 define("EMAIL_VERIFICATION_URL", "http://127.0.0.1/php-login/4-full-mvc-framework/login/verify");
-define("EMAIL_VERIFICATION_FROM", "noreply@example.com");
+define("EMAIL_VERIFICATION_FROM_EMAIL", "noreply@example.com");
+define("EMAIL_VERIFICATION_FROM_NAME", "My Project");
 define("EMAIL_VERIFICATION_SUBJECT", "Account Activation for PROJECT XY");
 define("EMAIL_VERIFICATION_CONTENT", "Please click on this link to activate your account: ");
