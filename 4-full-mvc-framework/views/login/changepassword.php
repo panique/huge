413dc19fa3d3a35bc5a54d38d157fb24ac23701b
Panique <panique@web.de>
2013-08-03 23:32:15 +0200

    all registration / password change views now show the min password length

--- 4-full-mvc-framework/views/login/changepassword.php
+++ 4-full-mvc-framework/views/login/changepassword.php
@@ -20,7 +20,7 @@
         <input type='hidden' name='user_name' value='<?php echo $this->user_name; ?>' />
         <input type='hidden' name='user_password_reset_hash' value='<?php echo $this->user_password_reset_hash; ?>' />
 
-        <label for="reset_input_password_new">New password</label>
+        <label for="reset_input_password_new">New password (min. 6 characters)</label>
         <input id="reset_input_password_new" class="reset_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />  
 
         <label for="reset_input_password_repeat">Repeat new password</label>
