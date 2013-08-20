8b340bc920b32981f3b6867a06ecf36d575087d0
Panique <panique@web.de>
2013-08-20 14:43:05 +0200

    [#165] better email links

--- 4-full-mvc-framework/config/config.php
+++ 4-full-mvc-framework/config/config.php
@@ -149,13 +149,13 @@ define("EMAIL_SMTP_ENCRYPTION", 'ssl');
  * define("EMAIL_VERIFICATION_SUBJECT", "Account Activation for PROJECT XY");
  * define("EMAIL_VERIFICATION_CONTENT", "Please click on this link to activate your account:");
  */
-define("EMAIL_PASSWORDRESET_URL", "http://127.0.0.1/php-login/4-full-mvc-framework/login/verifypasswordrequest");
+define("EMAIL_PASSWORDRESET_URL", URL . "login/verifypasswordrequest");
 define("EMAIL_PASSWORDRESET_FROM_EMAIL", "noreply@example.com");
 define("EMAIL_PASSWORDRESET_FROM_NAME", "My Project");
 define("EMAIL_PASSWORDRESET_SUBJECT", "Password reset for PROJECT XY");
 define("EMAIL_PASSWORDRESET_CONTENT", "Please click on this link to reset your password: ");
 
-define("EMAIL_VERIFICATION_URL", "http://127.0.0.1/php-login/4-full-mvc-framework/login/verify");
+define("EMAIL_VERIFICATION_URL", URL . "login/verify");
 define("EMAIL_VERIFICATION_FROM_EMAIL", "noreply@example.com");
 define("EMAIL_VERIFICATION_FROM_NAME", "My Project");
 define("EMAIL_VERIFICATION_SUBJECT", "Account Activation for PROJECT XY");
