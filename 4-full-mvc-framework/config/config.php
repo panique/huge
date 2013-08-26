fed1ed539024cfd35055c5ab5319603b8048eb02
Panique <panique@web.de>
2013-08-27 00:20:04 +0200

    explaining the COOKIE_DOMAIN better

--- 4-full-mvc-framework/config/config.php
+++ 4-full-mvc-framework/config/config.php
@@ -40,7 +40,8 @@ define('USE_GRAVATARS', false);
  * in a format like this: .mydomain.com
  * Note the . in front of the domain. No www, no http, no slash here!
  * For local development .127.0.0.1 is fine, but when deploying you should
- * change this to your real domain!
+ * change this to your real domain, like '.mydomain.com' ! The leading dot makes the cookie available for
+ * subdomains too.
  * @see php.net/manual/en/function.setcookie.php
  */
 define('COOKIE_RUNTIME', 1209600); // 1209600 seconds = 2 weeks
