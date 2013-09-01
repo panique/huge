688beca67ced386df4378e5a25b5c108e5dbb635
devplanete <devplanete@free.fr>
2013-09-01 17:17:58 +0200

    Create fr.php

---
+++ 2-advanced/lang/fr.php
@@ -0,0 +1,90 @@
+<?php
+
+$phplogin_lang = array(
+
+// Login & Registration classes
+'Database error'			=> 'Problème de connexion à la base de données.',
+'Empty username'			=> 'Le nom d\'utilisateur est vide',
+'Username exist'			=> 'Désolé, ce nom d\'utilisateur est déjà utilisé. Merci d\'en choisir un autre.',
+'Invalid username'			=> 'Nom d\'utilisateur invalide : seul les lettres a-Z et les chiffres sont autorisés, longueur de 2 à 64 caractères',
+'Empty password'			=> 'Le mot de passe est vide',
+'Bad confirm password'		=> 'Le 2 mots de passe ne sont pas identiques',
+'Password too short'		=> 'Le mot de passe doit contenir au minimum 6 caractères',
+'Email exist'				=> 'Cette adresse email est déjà enregistrée. Merci d\'utiliser le lien "J\'ai oublié mon mot de passe" si vous avez oublié votre mot de passe.',
+'Invalid email'				=> 'Cette adresse email n\'a pas un format valide',
+
+// Registration class
+'Wrong captcha'				=> 'Captcha was wrong!',
+'Username bad length'		=> 'Le nom d\utilisateur doit contenir entre 2 et 64 caractères',
+'Empty email'				=> 'L\'adresse email ne peut pas être vide',
+'Email too long'			=> 'L\'adresse email doit contenir au maximum 64 caractères',
+'Verification mail error'	=> 'Désolé, impossible de vous envoyez le mail de vérification. Votre compte n\'a pas été créé.',
+'Verification mail sent'	=> 'Un email de vérification vous a été envoyé !',
+'Verification mail not sent'=> 'Impossible de vous envoyez le mail de vérification ! Erreur : ',
+'Registration failed'		=> 'Désolé, une erreur s\'est produite durant votre enregistrement. Merci de recommencer.',
+'Activation successful'		=> 'Votre compte a été activé ! Vous pouvez maintenant vous connectez !',
+'Activation error'			=> 'Désolé, la combinaison id/code de vérification est invalide...',
+
+// Login class
+'Invalid cookie'			=> 'Cookie invalide',
+'User not exist'			=> 'Cet utilisateur n\'existe pas',
+'Wrong password'			=> 'Mot de passe incorrect. Essayez à nouveau.',
+'Account not activated'		=> 'Votre compte n\'est pas encore activé. Merci de cliquer sur le lien de confirmation dans le mail d\'enregistrement.',
+'Logged out'				=> 'Vous avez été deconnecté.',
+'Same username'				=> 'Sorry, that username is the same as your current one. Please choose another one.',
+'Same email'				=> 'Sorry, that email address is the same as your current one. Please choose another one.',
+'Username changed'			=> 'Votre nom d\'utilisateur a été changé. Votre nom d\'utilisateur est maintenant ',
+'Username change failed'	=> 'Désolé, une erreur s\'est produite durant l\'enregistrement de votre nouveau nom d\'utiisateur',
+'Email changed'				=> 'Votre adresse email a été changée. Votre addresse email est maintenant ',
+'Email change failed'		=> 'Désolé, une erreur s\'est produite durant l\'enregistrement de votre nouvelle adresse email',
+'Password changed'			=> 'Votre mot de passe a été changé !',
+'Password changed failed'	=> 'Désolé, une erreur s\'est produite durant l\'enregistrement de votre nouveau mot de passe.',
+'Wrong old password'		=> 'Votre ancien mot de passe n\'est pas correct.',
+'Password mail sent'		=> 'Le mail de réinitialisation de votre mot de passe a été envoyé !',
+'Password mail not sent'	=> 'Une erreur s\'est produite durant l\'envoi du mail de réinitialisation de votre mot de passe ! Erreur : ',
+'Reset link has expired'	=> 'Ce lien de réinitialisation du mot de passe est expiré. Il n\'est actif que pendant une heure.',
+'Empty link parameter'		=> 'Paramètre du lien incorrect.',
+
+// Login form
+'Username'					=> 'Nom d\'utilisateur',
+'Password'					=> 'Mot de passe',
+'Remember me'				=> 'Resté connecté (pour 2 semaines)',
+'Log in'					=> 'Connexion',
+'Register new account'		=> 'Créer un nouveau compte',
+'I forgot my password'		=> 'J\'ai oublié mon mot de passe',
+
+// Register form
+'Register username'			=> 'Nom d\'utilisateur (seulement des lettres et des chiffres, de 2 à 64 caractères)',
+'Register email'			=> 'E-mail (merci de fournir une adresse valide car vous recevrez un mail de vérification avec un lien d\'activation)',
+'Register password'			=> 'Mot de passe (min. 6 caractères)',
+'Register password repeat'	=> 'Mot de passe repéter',
+'Register captcha'			=> 'Saisir les caractères de l\'image ci-dessus',
+'Register'					=> 'S\'enregistrer',
+'Back to login'				=> 'Revenir à la page de connexion',
+
+// Password reset request
+'Password reset request'	=> 'Démander la réinitialisation de mon mot de passe. Saisir votre nom d\'utilisateur et vous recevrez un mail avec les instructions :',
+'Reset my password'			=> 'Réinitialiser mon mot de passe',
+'New password'				=> 'Nouveau mot de passe',
+'Repeat new password'		=> 'Repéter le nouveau mot de passe',
+'Submit new password'		=> 'Soumettre le nouveau mot de passe',
+
+// Edit account
+'Edit title'			=> 'Vous êtes connecté et pouvez modifier vos informations de connexion ici',
+'Old password'				=> 'Ancien mot de passe',
+'New username'				=> 'Nouveau nom d\'utilisateur (seul les lettres a-Z et les chiffres sont autorisés, longueur de 2 à 64 caractères)',
+'New email'					=> 'Nouvelle adresse email',
+'currently'					=> 'actuellement',
+'Change username'			=> 'Changer le nom d\'utilisateur',
+'Change email'				=> 'Changer l\'adresse email',
+'Change password'			=> 'Changer le mot de passe',
+
+// Logged in
+'You are logged in as'		=> 'Vous êtes conncté en tant que : ',
+'Logout'					=> 'Déconnexion',
+'Edit user data'			=> 'Modifier mon compte',
+'Profile picture'			=> 'Photo de profil (depuis gravatar) :'
+
+);
+
+?>
