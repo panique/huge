17a88cacb1558a8417cdc9e8f5e27b2c1d789426
Panique <panique@web.de>
2013-09-06 04:53:01 +0200

    deletion & complete rewrite of the 0-one-file project !!!

--- 0-one-file/index.php
+++ 0-one-file/index.php
@@ -9,33 +9,74 @@
 // TODO: "dont use else" rule ?
 // TODO: max level intend == 1 ?
 // TODO: PHP_SELF ?
-// TODO: out-of-the-box support for SQLite
+// TODO: explain the horrible missing of rowCount() in SQLite PDO !
+
+// no hard exit via exit()
 
 class Login
 {
-    private $db_connection = null;
-
+    /**
+     * @var string
+     */
     private $db_type = "sqlite"; // feel free to expend this with mysql etc.
+
+    /**
+     * @var string
+     */
     private $db_sqlite_path = "database/users.db";
 
-    // is THIS even used ?
-    private $user_name = "";
-    private $user_email = "";
-    private $user_password_hash = "";
+    /**
+     * @var null
+     */
+    private $db_connection = null;
+
+    /**
+     * @var bool
+     */
     private $user_is_logged_in = false;
 
+    /**
+     * @var string
+     */
     public $feedback = "";
 
 
+    /**
+     * Does necessary checks for PHP version and PHP password compatibility library and runs the application
+     */
     public function __construct()
     {
-        // perform necessary checks for PHP version and PHP password compatibility library
-        $this->performMinimumRequirementsCheck();
+        if ($this->performMinimumRequirementsCheck()) {
+            $this->runApplication();
+        }
     }
 
-    public function run()
+    /**
+     * Performs a check for minimum requirements to run this application.
+     * Does not run the further application when PHP version is lower than 5.3.7
+     * Does include the PHP password compatibility library when PHP version lower than 5.5.0
+     * (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+     * TODO: failsafe method flow would be nice (default return)
+     */
+    private function performMinimumRequirementsCheck()
     {
-        // TODO: get rid of 2 levels deep if/else ?
+        if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+            echo "Sorry, Simple PHP Login does not run on a PHP version older than 5.3.7 !";
+            return false;
+        } elseif (version_compare(PHP_VERSION, '5.5.0', '<')) {
+            require_once("libraries/password_compatibility_library.php");
+            return true;
+        } elseif (version_compare(PHP_VERSION, '5.5.0', '>=')) {
+            return true;
+        }
+    }
+
+    /**
+     * This is basically the Controller that handles the entire flow of the application.
+     * TODO: get rid of 2 levels deep if/else ?
+     */
+    public function runApplication()
+    {
         // check is user wants to see register page (etc.)
         if (isset($_GET["action"]) && $_GET["action"] == "register") {
             $this->doRegistration();
@@ -54,34 +95,27 @@ class Login
         }
     }
 
+    /**
+     * Creates a PDO database connection (in this case to a SQLite flat-file database)
+     * @return bool database creation success status
+     */
     private function createDatabaseConnection()
     {
         try {
             $this->db_connection = new PDO($this->db_type . ':' . $this->db_sqlite_path);
             return true;
         } catch (PDOException $e) {
-            // TODO: output error
-            $this->feedback = "Database connection problem.";
+            $this->feedback = "PDO database connection problem: " . $e->getMessage();
+            return false;
+        } catch (Exception $e) {
+            $this->feedback = "General problem: " . $e->getMessage();
             return false;
         }
     }
 
-    private function performMinimumRequirementsCheck()
-    {
-        // checking for minimum PHP version
-        if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-            exit("Sorry, Simple PHP Login does not run on a PHP version older than 5.3.7 !");
-        } elseif (version_compare(PHP_VERSION, '5.5.0', '<')) {
-            // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
-            // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
-            require_once("libraries/password_compatibility_library.php");
-        }
-    }
-
     /**
-     * 1. if user tried to log out
-     * 2. if user has an active session on the server
-     * 3. if user just submitted a login form
+     * Handles the flow of the login/logout process. According to the circumstances, a logout, a login with session
+     * data or a login with post data will be performed
      */
     private function performUserLoginAction()
     {
@@ -94,21 +128,26 @@ class Login
         }
     }
 
-    private function performUserRegisterAction()
-    {
-
-    }
-
+    /**
+     * Simply starts the session.
+     * It's cleaner to put this into a method than writing it directly into runApplication()
+     */
     private function doStartSession()
     {
         session_start();
     }
 
+    /**
+     *
+     */
     private function doLoginWithSessionData()
     {
         $this->user_is_logged_in = true; // ?
     }
 
+    /**
+     * TODO: split checkLoginFormDataPasswordCorrect into CHECK for login data and REALLY LOGGIN IN
+     */
     private function doLoginWithPostData()
     {
         // TODO: how to fix 2 levels deep if structure
@@ -120,6 +159,9 @@ class Login
         }
     }
 
+    /**
+     * Logs the user out
+     */
     private function doLogout()
     {
         $_SESSION = array();
@@ -129,6 +171,10 @@ class Login
         $this->feedback = "You were just logged out.";
     }
 
+    /**
+     * TODO
+     * @return bool
+     */
     private function doRegistration()
     {
         if ($this->checkRegistrationData()) {
@@ -140,9 +186,12 @@ class Login
         return false;
     }
 
+    /**
+     * Validates the login form data, checks if username and password are provided
+     * @return bool
+     */
     private function checkLoginFormDataNotEmpty()
     {
-        // if POST data (from login form) contains non-empty user_name and non-empty user_password
         if (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
             return true;
         } elseif (empty($_POST['user_name'])) {
@@ -150,10 +199,15 @@ class Login
         } elseif (empty($_POST['user_password'])) {
             $this->feedback = "Password field was empty.";
         }
+        // default return
         return false;
     }
 
     // TODO: remove 2 levels deep if structure
+    /**
+     * Checks if user exits, if so: check if provided password matches the one in the database
+     * @return bool user login status
+     */
     private function checkLoginFormDataPasswordCorrect()
     {
         $sql = 'SELECT user_name, user_email, user_password_hash FROM users WHERE user_name = :user_name LIMIT 1';
@@ -162,7 +216,7 @@ class Login
         $query->execute();
 
         // btw that's the weird way to get num_rows in PDO with SQLite. what a fucking bullshit! but that's the
-        // way to get the rows. $result->numRows() works with SQlite pure, but not with SQLite PDO.
+        // way to get the rows. $result->numRows() works with SQLite pure, but not with SQLite PDO.
         // I think that PDO is a bad choice.
         //if (count($query->fetchAll(PDO::FETCH_NUM)) == 1) {
 
@@ -188,14 +242,32 @@ class Login
         }
     }
 
+    /**
+     * Validates the user's registration input
+     * @return bool success status of user's registration data validation
+     */
     private function checkRegistrationData()
     {
-        // if not registration form submitted, exit the method
+        // if no registration form submitted: exit the method
         if (!isset($_POST["register"])) {
             return false;
         }
 
-        if (empty($_POST['user_name'])) {
+        // validating the input
+        if (!empty($_POST['user_name'])
+            && strlen($_POST['user_name']) <= 64
+            && strlen($_POST['user_name']) >= 2
+            && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
+            && !empty($_POST['user_email'])
+            && strlen($_POST['user_email']) <= 64
+            && filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)
+            && !empty($_POST['user_password_new'])
+            && !empty($_POST['user_password_repeat'])
+            && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
+        ) {
+            // only this case return true, only this case is valid
+            return true;
+        } elseif (empty($_POST['user_name'])) {
             $this->feedback = "Empty Username";
         } elseif (empty($_POST['user_password_new']) || empty($_POST['user_password_repeat'])) {
             $this->feedback = "Empty Password";
@@ -213,19 +285,6 @@ class Login
             $this->feedback = "Email cannot be longer than 64 characters";
         } elseif (!filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)) {
             $this->feedback = "Your email address is not in a valid email format";
-        } elseif (!empty($_POST['user_name'])
-            && strlen($_POST['user_name']) <= 64
-            && strlen($_POST['user_name']) >= 2
-            && preg_match('/^[a-z\d]{2,64}$/i', $_POST['user_name'])
-            && !empty($_POST['user_email'])
-            && strlen($_POST['user_email']) <= 64
-            && filter_var($_POST['user_email'], FILTER_VALIDATE_EMAIL)
-            && !empty($_POST['user_password_new'])
-            && !empty($_POST['user_password_repeat'])
-            && ($_POST['user_password_new'] === $_POST['user_password_repeat'])
-        ) {
-            // only case that's valid
-            return true;
         } else {
             $this->feedback = "An unknown error occurred.";
         }
@@ -279,29 +338,43 @@ class Login
     }
 
     /**
-     * simply return the current state of the user's login
-     * @return boolean user's login status
+     * Simply returns the current status of the user's login
+     * @return bool user's login status
      */
     public function getUserLoginStatus()
     {
         return $this->user_is_logged_in;
     }
 
+    /**
+     * Simple demo-"page" that will be shown when the user is logged in.
+     * In a real application you would probably include an html-template here, but for this extremely simple
+     * demo the "echo" statements are totally okay.
+     */
     private function showPageLoggedIn()
     {
         if ($this->feedback) {
             echo $this->feedback . "<br/><br/>";
         }
 
+        // TODO: should we include a template here ?
+
         echo 'Hello ' . $_SESSION['user_name'] . ', you are logged in.<br/><br/>';
         echo '<a href="' . $_SERVER['PHP_SELF'] . '?action=logout">Log out</a>';
     }
 
+    /**
+     * Simple demo-"page" with the login form.
+     * In a real application you would probably include an html-template here, but for this extremely simple
+     * demo the "echo" statements are totally okay.
+     */
     private function showPageLoginForm()
     {
         if ($this->feedback) {
             echo $this->feedback . "<br/><br/>";
         }
+
+        // TODO: should we include a template here ?
 
         echo '<h2>Login</h2>';
 
@@ -318,11 +391,18 @@ class Login
         echo '<a href="' . $_SERVER['PHP_SELF'] . '?action=register">Register new account</a>';
     }
 
+    /**
+     * Simple demo-"page" with the registration form.
+     * In a real application you would probably include an html-template here, but for this extremely simple
+     * demo the "echo" statements are totally okay.
+     */
     private function showPageRegistration()
     {
         if ($this->feedback) {
             echo $this->feedback . "<br/><br/>";
         }
+
+        // TODO: should we include a template here ?
 
         echo '<h2>Registration</h2>';
 
@@ -339,10 +419,8 @@ class Login
         echo '</form>';
 
         echo '<a href="' . $_SERVER['PHP_SELF'] . '">Homepage</a>';
-
     }
 }
 
 // runs the app
 $login = new Login();
-$login->run();
