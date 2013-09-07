f45b0200b092a8bc61539174431e90c708547751
devplanete <devplanete@free.fr>
2013-09-07 12:16:43 +0200

    Update register.php: merged html form

--- 2-advanced/register.php
+++ 2-advanced/register.php
@@ -17,11 +17,46 @@
  */
 
 // load php-login components
-require_once("php-login.php");
+require_once('php-login.php');
 
-// create the registration object. when this object is created, it will do all registration stuff automatically
+// create the login object. when this object is created, it will do all registration stuff automatically
 // so this single line handles the entire registration process.
-$registration = new Registration();
+$login = new Registration();
+
+// include html header and display php-login message/error
+include('includes/header.php');
 
 // showing the register view (with the registration form, and messages/errors)
-include("views/register.php");
+// show register form
+// - the user name input field uses a HTML5 pattern check
+// - the email input field uses a HTML5 email type check
+if (!$login->registration_successful && !$login->verification_successful) { ?>
+
+<form method="post" action="register.php" name="registerform">   
+    <label for="user_name"><?php echo $phplogin_lang['Register username']; ?></label>
+    <input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+
+    <label for="user_email"><?php echo $phplogin_lang['Register email']; ?></label>
+    <input id="user_email" type="email" name="user_email" required />
+
+    <label for="user_password_new"><?php echo $phplogin_lang['Register password']; ?></label>
+    <input id="user_password_new" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
+
+    <label for="user_password_repeat"><?php echo $phplogin_lang['Register password repeat']; ?></label>
+    <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />       
+
+    <img src="register_captcha.php" alt="captcha" style="display: block;"/>
+
+    <label><?php echo $phplogin_lang['Register captcha']; ?></label>
+    <input type="text" name="captcha" required />
+
+    <input type="submit" name="register" value="<?php echo $phplogin_lang['Register']; ?>" />
+</form>
+
+<?php } ?>
+
+<a href="index.php"><?php echo $phplogin_lang['Back to login']; ?></a>
+
+<?php
+// include html footer
+include('includes/footer.php');
