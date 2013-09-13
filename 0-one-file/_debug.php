87eea24de1ad088e9a61e3ecc7b0b83a59198fea
Panique <panique@web.de>
2013-09-13 14:39:38 +0200

    optimization

--- 0-one-file/_debug.php
+++ 0-one-file/_debug.php
@@ -5,15 +5,22 @@
  * Might be useful for your development.
  */
 
+// error reporting config
 error_reporting(E_ALL);
 
+// config
 $db_type = "sqlite";
 $db_sqlite_path = "database/users.db";
 
+// create new database connection
 $db_connection = new PDO($db_type . ':' . $db_sqlite_path);
 
+// query
 $sql = 'SELECT * FROM users';
+
+// execute query
 $query = $db_connection->prepare($sql);
 $query->execute();
 
+// show all the data from the "users" table inside the database
 var_dump($query->fetchAll());
--- 0-one-file/_install.php
+++ 0-one-file/_install.php
@@ -5,13 +5,17 @@
  * It simply creates a new and empty database.
  */
 
+// error reporting config
 error_reporting(E_ALL);
 
+// config
 $db_type = "sqlite";
 $db_sqlite_path = "database/users.db";
 
+// create new database file / connection (the file will be automaticly created the first time a connection is made up)
 $db_connection = new PDO($db_type . ':' . $db_sqlite_path);
 
+// create new empty table inside the database (if table does not already exist)
 $sql = 'CREATE TABLE IF NOT EXISTS `users` (
         `user_id` INTEGER PRIMARY KEY,
         `user_name` varchar(64),
@@ -19,5 +23,13 @@ $sql = 'CREATE TABLE IF NOT EXISTS `users` (
         `user_email` varchar(64));
         CREATE UNIQUE INDEX `user_name_UNIQUE` ON `users` (`user_name` ASC);';
 
+// execute the above query
 $query = $db_connection->prepare($sql);
 $query->execute();
+
+// check for success
+if (file_exists($db_sqlite_path)) {
+    echo "Database $db_sqlite_path exists, installation was successful.";
+} else {
+    echo "Database $db_sqlite_path does not exist, installation was NOT successful. Missing folder write rights ?";
+}
