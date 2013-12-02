558c65ae4733ea99cf524dbe5f72a230f5b12893
Dominic <dominic-ts@gmx.de>
2013-12-02 03:06:12 +0100

    case sensitive error overview.php

--- 4-full-mvc-framework/application/controllers/overview.php
+++ 4-full-mvc-framework/application/controllers/overview.php
@@ -15,14 +15,14 @@ class Overview extends Controller
     {
         // get all users that exist and put the result into $this->view->users
         // in the view this is available in $this->users
-        $overview_model = $this->loadModel('Overview');
+        $overview_model = $this->loadModel('overview');
         $this->view->users = $overview_model->getAllUsersProfiles();
         $this->view->render('overview/index');
     }
 
     function showuserprofile($user_id)
     {
-        $overview_model = $this->loadModel('Overview');
+        $overview_model = $this->loadModel('overview');
         $this->view->user = $overview_model->getUserProfile($user_id);
         $this->view->render('overview/showuserprofile');
     }
