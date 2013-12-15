63110aa3e7f73669d2b6e134b18dfef97c2e56ae
Panique <panique@web.de>
2013-12-15 05:19:07 +0100

    small improvements

--- 4-full-framework/application/controllers/dashboard.php
+++ 4-full-framework/application/controllers/dashboard.php
@@ -11,7 +11,7 @@ class Dashboard extends Controller
     {
         // a little note on that (seen on StackOverflow):
         // "As long as myChild has no constructor, the parent constructor will be called / inherited."
-        // This means wenn a class thats extends another class has a __construct, it needs to construct
+        // This means when a class that extends another class has a __construct, it needs to construct
         // the parent in that constructor, like this:
 		parent::__construct();
                 
