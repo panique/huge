c7f641b5e2f86e36006c72e0a0c52edb633de8bf
Panique <panique@web.de>
2013-12-22 23:06:26 +0100

    improvements

--- application/views/_templates/feedback.php
+++ application/views/_templates/feedback.php
@@ -1,6 +1,6 @@
 <?php
 
-// get the feedbacks (they are arrays, to make multiple positive/negative messages possible)
+// get the feedback (they are arrays, to make multiple positive/negative messages possible)
 $feedback_positive = Session::get('feedback_positive');
 $feedback_negative = Session::get('feedback_negative');
 
