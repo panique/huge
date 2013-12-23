f151daf50b5192d3e03bd2802a0a4a27f1643fe7
Panique <panique@web.de>
2013-12-23 06:24:03 +0100

    smaller file name corrections

--- application/views/_templates/header.php
+++ application/views/_templates/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo URL; ?>public/css/reset.css" />
-	<link rel="stylesheet" href="<?php echo URL; ?>public/css/default.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css" />
     <!-- in case you wonder: That's the cool-kids-protocol-free way to load jQuery -->
 	<script type="text/javascript" src="//code.jquery.com/jquery-2.0.3.min.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/application.js"></script>
