837c7006102a5580b6ce05966a3f989b59a2574c
Panique <panique@web.de>
2013-12-23 06:20:58 +0100

    smaller comment corrections

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,5 +1,5 @@
     <div class="footer">
-        <!-- echo out the content of the SESSION via KINT, a Composer loaded much better version of var_dump -->
+        <!-- echo out the content of the SESSION via KINT, a Composer-loaded much better version of var_dump -->
         <!-- KINT can be used with the simple function d() -->
         <?php d($_SESSION); ?>
     </div>
