37f4c8d9fe95a9d85858583f8ad12dc42fd5c536
Panique <panique@web.de>
2013-12-26 02:06:16 +0100

    login method improvements

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -21,111 +21,121 @@ class LoginModel
     /**
      * Login process (for DEFAULT user accounts).
      * Users who register/login with Facebook etc. are handled with loginWithFacebook()
-     * TODO: hardcore refactoring
      * @return bool success state
      */
     public function login()
     {
-        if (!empty($_POST['user_name']) && !empty($_POST['user_password'])) {
-            // get user's data
-            // (we check if the password fits the password_hash via password_verify() some lines below)
-            $sth = $this->db->prepare("SELECT user_id, 
-                                              user_name, 
-                                              user_email, 
-                                              user_password_hash, 
-                                              user_active, 
-                                              user_account_type,
-                                              user_failed_logins, 
-                                              user_last_failed_login  
-                                       FROM   users
-                                       WHERE  user_name = :user_name
-                                              OR user_email = :user_name
-                                              AND user_provider_type = :provider_type");
-            // DEFAULT is the marker for "normal" accounts (that have a password etc.)
-            // There are other types of accounts that don't have passwords etc. (FACEBOOK)
-            $sth->execute(array(':user_name' => $_POST['user_name'], ':provider_type' => 'DEFAULT'));
+        // we do negative-first checks here
+        if (!isset($_POST['user_name']) OR empty($_POST['user_name'])) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_USERNAME_FIELD_EMPTY;
+            return false;
+        }
+        if (!isset($_POST['user_password']) OR empty($_POST['user_password'])) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_FIELD_EMPTY;
+            return false;
+        }
 
-            $count =  $sth->rowCount();
-            if ($count == 1) {
-                // fetch one row (we only have one result)
-                $result = $sth->fetch();
-                
-                if ( ($result->user_failed_logins >= 3) && ($result->user_last_failed_login > (time()-30)) ) {
-                    $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_WRONG_3_TIMES;
-                    return false;
-                } else {
+        // get user's data
+        // (we check if the password fits the password_hash via password_verify() some lines below)
+        $sth = $this->db->prepare("SELECT user_id,
+                                          user_name,
+                                          user_email,
+                                          user_password_hash,
+                                          user_active,
+                                          user_account_type,
+                                          user_failed_logins,
+                                          user_last_failed_login
+                                   FROM   users
+                                   WHERE  (user_name = :user_name OR user_email = :user_name)
+                                          AND user_provider_type = :provider_type");
+        // DEFAULT is the marker for "normal" accounts (that have a password etc.)
+        // There are other types of accounts that don't have passwords etc. (FACEBOOK)
+        $sth->execute(array(':user_name' => $_POST['user_name'], ':provider_type' => 'DEFAULT'));
+        $count =  $sth->rowCount();
+        // if there's NOT one result
+        if ($count != 1) {
+            // was FEEDBACK_USER_DOES_NOT_EXIST before, but has changed to FEEDBACK_LOGIN_FAILED
+            // to prevent potential attackers showing if the user exists
+            $_SESSION["feedback_negative"][] = FEEDBACK_LOGIN_FAILED;
+            return false;
+        }
 
-                    if (password_verify($_POST['user_password'], $result->user_password_hash)) {
+        // fetch one row (we only have one result)
+        $result = $sth->fetch();
 
-                        if ($result->user_active == 1) {
-                            // login
-                            Session::init();
-                            Session::set('user_logged_in', true);
-                            Session::set('user_id', $result->user_id);
-                            Session::set('user_name', $result->user_name);
-                            Session::set('user_email', $result->user_email);
-                            Session::set('user_account_type', $result->user_account_type);
-                            Session::set('user_provider_type', 'DEFAULT');
-                            
-                            Session::set('user_avatar_file', $this->getUserAvatarFilePath());
+        // block login attempt if somebody has already failed 3 times and the last login attempt is less than 30sec ago
+        if (($result->user_failed_logins >= 3) AND ($result->user_last_failed_login > (time()-30))) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_WRONG_3_TIMES;
+            return false;
+        }
 
-                            // call the setGravatarImageUrl() method which writes gravatar urls into the session
-                            $this->setGravatarImageUrl($result->user_email, AVATAR_SIZE);
+        // check if hash of provided password matches the hash in the database
+        if (password_verify($_POST['user_password'], $result->user_password_hash)) {
 
-                            // reset the failed login counter for that user
-                            $sql = "UPDATE users
-                                    SET user_failed_logins = 0, user_last_failed_login = NULL
-                                    WHERE user_id = :user_id AND user_failed_logins != 0";
-                            $sth = $this->db->prepare($sql);
-                            $sth->execute(array(':user_id' => $result->user_id));
-                            
-                            // if user has check the "remember me" checkbox, then write cookie
-                            if (isset($_POST['user_rememberme'])) {
-                                
-                                // generate 64 char random string
-                                $random_token_string = hash('sha256', mt_rand());
-                                
-                                $sth = $this->db->prepare("UPDATE users SET user_rememberme_token = :user_rememberme_token WHERE user_id = :user_id");
-                                $sth->execute(array(':user_rememberme_token' => $random_token_string, ':user_id' => $result->user_id));
-                                
-                                // generate cookie string that consists of userid, randomstring and combined hash of both
-                                $cookie_string_first_part = $result->user_id . ':' . $random_token_string;
-                                $cookie_string_hash = hash('sha256', $cookie_string_first_part);        
-                                $cookie_string = $cookie_string_first_part . ':' . $cookie_string_hash;        
-
-                                // set cookie
-                                setcookie('rememberme', $cookie_string, time() + COOKIE_RUNTIME, "/", COOKIE_DOMAIN);
-                            }
-
-                            return true;
-                        } else {
-
-                            $_SESSION["feedback_negative"][] = FEEDBACK_ACCOUNT_NOT_ACTIVATED_YET;
-                            return false;
-                        }
-                    } else {
-
-                        // increment the failed login counter for that user
-                        $sth = $this->db->prepare("UPDATE users "
-                                . "SET user_failed_logins = user_failed_logins+1, user_last_failed_login = :user_last_failed_login "
-                                . "WHERE user_name = :user_name OR user_email = :user_name");
-                        $sth->execute(array(':user_name' => $_POST['user_name'], ':user_last_failed_login' => time() ));
-
-                        $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_WRONG;
-                        return false;
-                    }
-                }
-            } else {
-                // was FEEDBACK_USER_DOES_NOT_EXIST before, but has changed to FEEDBACK_LOGIN_FAILED
-                // to prevent potential attackers showing if the user exists
-                $_SESSION["feedback_negative"][] = FEEDBACK_LOGIN_FAILED;
+            if ($result->user_active != 1) {
+                $_SESSION["feedback_negative"][] = FEEDBACK_ACCOUNT_NOT_ACTIVATED_YET;
                 return false;
             }
-        } elseif (empty($_POST['user_name'])) {
-            $_SESSION["feedback_negative"][] = FEEDBACK_USERNAME_FIELD_EMPTY;
-        } elseif (empty($_POST['user_password'])) {
-            $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_FIELD_EMPTY;
+
+            // login process, write the user data into session
+            Session::init();
+            Session::set('user_logged_in', true);
+            Session::set('user_id', $result->user_id);
+            Session::set('user_name', $result->user_name);
+            Session::set('user_email', $result->user_email);
+            Session::set('user_account_type', $result->user_account_type);
+            Session::set('user_provider_type', 'DEFAULT');
+            // put native avatar path into session
+            Session::set('user_avatar_file', $this->getUserAvatarFilePath());
+            // put Gravatar URL into session
+            $this->setGravatarImageUrl($result->user_email, AVATAR_SIZE);
+
+            // reset the failed login counter for that user (if necessary)
+            if ($result->user_last_failed_login > 0) {
+                $sql = "UPDATE users
+                        SET user_failed_logins = 0, user_last_failed_login = NULL
+                        WHERE user_id = :user_id AND user_failed_logins != 0";
+                $sth = $this->db->prepare($sql);
+                $sth->execute(array(':user_id' => $result->user_id));
+            }
+
+            // if user has checked the "remember me" checkbox, then write cookie
+            if (isset($_POST['user_rememberme'])) {
+
+                // generate 64 char random string
+                $random_token_string = hash('sha256', mt_rand());
+
+                // write that token into database
+                $sql = "UPDATE users SET user_rememberme_token = :user_rememberme_token WHERE user_id = :user_id";
+                $sth = $this->db->prepare($sql);
+                $sth->execute(array(':user_rememberme_token' => $random_token_string, ':user_id' => $result->user_id));
+
+                // generate cookie string that consists of user id, random string and combined hash of both
+                $cookie_string_first_part = $result->user_id . ':' . $random_token_string;
+                $cookie_string_hash = hash('sha256', $cookie_string_first_part);
+                $cookie_string = $cookie_string_first_part . ':' . $cookie_string_hash;
+
+                // set cookie
+                setcookie('rememberme', $cookie_string, time() + COOKIE_RUNTIME, "/", COOKIE_DOMAIN);
+            }
+
+            // return true to make clear the login was successful
+            return true;
+
+        } else {
+            // increment the failed login counter for that user
+            $sql = "UPDATE users
+                    SET user_failed_logins = user_failed_logins+1, user_last_failed_login = :user_last_failed_login
+                    WHERE user_name = :user_name OR user_email = :user_name";
+            $sth = $this->db->prepare($sql);
+            $sth->execute(array(':user_name' => $_POST['user_name'], ':user_last_failed_login' => time() ));
+            // feedback message
+            $_SESSION["feedback_negative"][] = FEEDBACK_PASSWORD_WRONG;
+            return false;
         }
+
+        // default return
+        return false;
     }
 
     /**
@@ -136,6 +146,7 @@ class LoginModel
     {
         $cookie = isset($_COOKIE['rememberme']) ? $_COOKIE['rememberme'] : '';
 
+        // do we have a cookie var ?
         if (!$cookie) {
             $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
             return false;
