646e1b95950a990c5e7ebc3e1b4ca2a9fd2a7279
Panique <panique@web.de>
2013-12-26 01:46:21 +0100

    loginCookie method refactoring

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -130,72 +130,62 @@ class LoginModel
 
     /**
      * performs the login via cookie (for DEFAULT user account, FACEBOOK-accounts are handled differently)
-     * TODO: hardcore refactoring
      * @return bool success state
      */
     public function loginWithCookie()
     {
         $cookie = isset($_COOKIE['rememberme']) ? $_COOKIE['rememberme'] : '';
 
-        if ($cookie) {
-            list ($user_id, $token, $hash) = explode(':', $cookie);
-            if ($hash !== hash('sha256', $user_id . ':' . $token)) {
-                $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
-                return false;
-            }
+        if (!$cookie) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
+            return false;
+        }
 
-            // do not log in when token is empty
-            if (empty($token)) {
-                $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
-                return false;
-            }
+        // check cookie's contents, check if cookie contents belong together
+        list ($user_id, $token, $hash) = explode(':', $cookie);
+        if ($hash !== hash('sha256', $user_id . ':' . $token)) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
+            return false;
+        }
 
-            // get real token from database (and all other data)
-            $sth = $this->db->prepare("SELECT user_id,
-                                              user_name,
-                                              user_email,
-                                              user_password_hash,
-                                              user_active,
-                                              user_account_type,
-                                              user_has_avatar,
-                                              user_failed_logins,
-                                              user_last_failed_login
-                                         FROM users
-                                         WHERE user_id = :user_id
-                                           AND user_rememberme_token = :user_rememberme_token
-                                           AND user_rememberme_token IS NOT NULL
-                                           AND user_provider_type = :provider_type");
-            $sth->execute(array(':user_id' => $user_id,
-                                ':user_rememberme_token' => $token,
-                                ':provider_type' => 'DEFAULT'));
+        // do not log in when token is empty
+        if (empty($token)) {
+            $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
+            return false;
+        }
 
-            $count =  $sth->rowCount();
-            if ($count == 1) {
-                // fetch one row (we only have one result)
-                $result = $sth->fetch();
-                // TODO: this block is same/similar to the one from login(), maybe we should put this in a method
-                // write data into session
-                Session::init();
-                Session::set('user_logged_in', true);
-                Session::set('user_id', $result->user_id);
-                Session::set('user_name', $result->user_name);
-                Session::set('user_email', $result->user_email);
-                Session::set('user_account_type', $result->user_account_type);
-                Session::set('user_provider_type', 'DEFAULT');
-                Session::set('user_avatar_file', $this->getUserAvatarFilePath());
+        // get real token from database (and all other data)
+        $query = $this->db->prepare("SELECT user_id, user_name, user_email, user_password_hash, user_active,
+                                          user_account_type,  user_has_avatar, user_failed_logins, user_last_failed_login
+                                     FROM users
+                                     WHERE user_id = :user_id
+                                       AND user_rememberme_token = :user_rememberme_token
+                                       AND user_rememberme_token IS NOT NULL
+                                       AND user_provider_type = :provider_type");
+        $query->execute(array(':user_id' => $user_id, ':user_rememberme_token' => $token, ':provider_type' => 'DEFAULT'));
+        $count =  $query->rowCount();
+        if ($count == 1) {
+            // fetch one row (we only have one result)
+            $result = $query->fetch();
+            // TODO: this block is same/similar to the one from login(), maybe we should put this in a method
+            // write data into session
+            Session::init();
+            Session::set('user_logged_in', true);
+            Session::set('user_id', $result->user_id);
+            Session::set('user_name', $result->user_name);
+            Session::set('user_email', $result->user_email);
+            Session::set('user_account_type', $result->user_account_type);
+            Session::set('user_provider_type', 'DEFAULT');
+            Session::set('user_avatar_file', $this->getUserAvatarFilePath());
 
-                // call the setGravatarImageUrl() method which writes gravatar urls into the session
-                $this->setGravatarImageUrl($result->user_email, AVATAR_SIZE);
+            // call the setGravatarImageUrl() method which writes gravatar urls into the session
+            $this->setGravatarImageUrl($result->user_email, AVATAR_SIZE);
 
-                // NOTE: we don't set another rememberme-cookie here as the current cookie should always
-                // be invalid after a certain amount of time, so the user has to login with username/password
-                // again from time to time. This is good and safe ! ;)
-                $_SESSION["feedback_positive"][] = FEEDBACK_COOKIE_LOGIN_SUCCESSFUL;
-                return true;
-            } else {
-                $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
-                return false;
-            }
+            // NOTE: we don't set another rememberme-cookie here as the current cookie should always
+            // be invalid after a certain amount of time, so the user has to login with username/password
+            // again from time to time. This is good and safe ! ;)
+            $_SESSION["feedback_positive"][] = FEEDBACK_COOKIE_LOGIN_SUCCESSFUL;
+            return true;
         } else {
             $_SESSION["feedback_negative"][] = FEEDBACK_COOKIE_INVALID;
             return false;
