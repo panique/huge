69df807024019c40570633f5eef6c7cfda417840
Panique <panique@web.de>
2014-08-10 03:38:59 +0200

    cURL fix for issue #453

--- application/libs/Auth.php
+++ application/libs/Auth.php
@@ -17,6 +17,9 @@ class Auth
         if (!isset($_SESSION['user_logged_in'])) {
             Session::destroy();
             header('location: ' . URL . 'login');
+            // to prevent fetching views via cURL (which "ignores" the header-redirect above) we leave the application
+            // the hard way, via exit(). @see https://github.com/panique/php-login/issues/453
+            exit();
         }
     }
 }
