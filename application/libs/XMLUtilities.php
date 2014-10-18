fffabbe479fd77f189a48f9fb13e5a401f30e713
TristanV <tristan.vanrullen@gmail.com>
2014-10-19 01:48:11 +0200

    little cleanup

--- application/libs/XMLUtilities.php
+++ application/libs/XMLUtilities.php
@@ -20,30 +20,29 @@ class XMLUtilities
 	 * @param string $xmlstr
 	 * @return array
 	 * @author Tristan Vanrullen (corrections to handle multiple elements having the same name)
+	 * based on work published by :
 	 * @author Adrien aka Gaarf
 	 */
 	
 	public static function array_to_xmlstr($array,$currentElement="XML",$level=0)
 	{
-		//	echo "\n<br/>ENTERING ARRAY TO XML for ELEMENT $currentElement ";
-		//	print_r($array);
-		//	echo "\n<br/>-----------";
 		$quote="\"";
 		$newline="\r\n";
 		$s="";
 		$space="";
 		for ($i=0;$i<$level;$i++) $space.="   ";
-		if (is_array($array) && isset($array['@multiple'])) // si cet �l�ment est multiple, on rappelle r�cursivement sur l'ensemble de ses entr�es
+		// if the element is multiple, then we call recursively the method over all its elements
+		if (is_array($array) && isset($array['@multiple'])) 
 		{
 			//		echo "\n<br/>ELEMENT $currentElement is multiple !";
-			foreach ($array as $val) //on parcourt chaque entr�e de l'�l�ment courant
+			foreach ($array as $val) //for each subelement
 			{
-				if (is_array($val))//�liminons les �l�ments fonctionnels
+				if (is_array($val))//get rid of functional elements
 				{
 					//$s.=array_to_xmlstr($val,$currentElement,true);
 					//element -----------------------
 					$s.="$space<$currentElement";
-					if (is_array($val)) //si cette entr�e est un tableau
+					if (is_array($val)) //if the value is an array
 					{
 						//attributs -----------------------
 						if (isset($val['@attributes']) && is_array($val['@attributes']))
@@ -65,16 +64,15 @@ class XMLUtilities
 						}
 						else $s.="/>$newline";
 					}
-					else if ($val!="") //si cette entr�e est une chaine
+					else if ($val!="") //if the value is a string
 					{
 						$s.=">";
 						$s.=$val;
 						$s.="$space</$currentElement>$newline";
 					}
-					else $s.="/>$newline"; //si cette entr�e est vide
+					else $s.="/>$newline"; //if the value is empty
 				}
 			}
-			//		echo "\n<br/>FIN MULTIPLE ELEMENT $currentElement !";
 		}
 		else if (!is_array($array))
 		{
@@ -83,49 +81,39 @@ class XMLUtilities
 			else
 				$s.="$space<$currentElement/>$newline";
 		}
-		else //on est en situation "standard" c.a.d. qu'on a des �l�ments directement comme cl�s du tableau
+		else //standard situation : elements are keys of the array
 		{
-			//		echo "\n<br/>ELEMENT $currentElement is standard !";
 			$s.="$space<".$currentElement;
 			if (isset($array['@attributes']) && is_array($array['@attributes']))
 			{
-				//			echo "\n<br/>ELEMENT $currentElement has attributes!";
 				foreach ($array['@attributes'] as $att=>$attval)
 				{
 					$s.=" ".$att."=$quote".$attval."$quote";
 				}
 			}
-			//sub elements �ventuels
+			//sub elements 
 			if ((isset($array['@attributes']) && count($array)>1)
 			|| (!isset($array['@attributes']) && count($array)>0))
 			{
 				$s.=">$newline";
-				foreach ($array as $key=>$val) //on parcourt chaque �l�ment de l'�l�ment courant
+				foreach ($array as $key=>$val)
 				{
-					if ($key=="@attributes") continue;//�liminons les �l�ments fonctionnels
-					//				echo "\n<br/>TEST SUBELEMENT $key for ELEMENT $currentElement:";
-					if (is_array($val) && isset($val['@multiple'])) // si cet �l�ment est multiple, on rappelle r�cursivement sur l'ensemble de ses entr�es
+					if ($key=="@attributes") continue;
+					if (is_array($val) && isset($val['@multiple'])) 
 					{
-						//					echo "\n<br/>ELEMENT $key is multiple !!!!";
 						$s.=array_to_xmlstr($val,$key,$level+1);
-					}//fin situation multiple
-					else //si cet �l�ment est non multiple
+					}
+					else 
 					{
-						//					echo "\n<br/>ELEMENT $key is solo!!!!";
 						$s.=array_to_xmlstr($val,$key,$level+1);
-					}// fin si situation non multiple
-					//				echo "\n<br/>FIN TEST SUBELEMENT $key for ELEMENT $currentElement:";
-				}//fin pour chaque �l�ment
+					}
+				}
 				$s.="$space</$currentElement>$newline";
 			}
 			else $s.="/>$newline";
-		}//fin situation standard
-		//	echo "\n<br/>LEAVING ARRAY TO XML";
-		//	echo (nl2br($s));
-		//	echo "\n<br/>-----------";
-	
+		}//end standard situation	
 		return $s;
-	}//fin array to xmlstr
+	}
 	
 	/**
 	 * convert an XML structure string into an array
@@ -174,7 +162,6 @@ class XMLUtilities
 		{
 			case XML_CDATA_SECTION_NODE:
 			case XML_TEXT_NODE:
-				//$output = htmlspecialchars(trim($node->textContent),ENT_NOQUOTES,$charset);
 				$output = htmlspecialchars(trim($node->textContent),ENT_NOQUOTES);
 				break;
 			case XML_ELEMENT_NODE:
@@ -207,29 +194,24 @@ class XMLUtilities
 					if(is_array($output))
 					{
 						if($node->attributes->length)
+						{
+							$a = array();
+							foreach($node->attributes as $attrName => $attrNode)
 							{
-								$a = array();
-								foreach($node->attributes as $attrName => $attrNode)
-								{
 								$a[$attrName] =
-								//htmlspecialchars(utf8_decode((string) $attrNode->value),ENT_NOQUOTES,$charset);
 								htmlspecialchars((string) $attrNode->value,ENT_NOQUOTES);
-								}
-								$output['@attributes'] = $a;
-								}
-								foreach ($output as $t => $v)
-								{
-								if(is_array($v) && count($v)==1 && $t!='@attributes')
-								{
-								$output[$t] = $v[0];
-								}
-								/*else if(is_array($v) && count($v)==0)
-								{
-									$output[$t] = (string)"";
-								}*/
-								}
 							}
-						break;
+							$output['@attributes'] = $a;
+						}
+						foreach ($output as $t => $v)
+						{
+							if(is_array($v) && count($v)==1 && $t!='@attributes')
+							{
+								$output[$t] = $v[0];
+							}
+						}
+					}
+					break;
 				}
 		return $output;
 	}
@@ -256,7 +238,6 @@ class XMLUtilities
 			throw new Exception("file $xmlfile couldn't be opened! ".$e->getMessage());
 		}
 		if (isset($docxml->xmlEncoding)) $charset=$docxml->xmlEncoding;
-		//echo "<br/>loading ".$xmlfile;
 		return XMLUtilities::domnode_to_associative_array($docxml->documentElement,$charset,$element_path,$key_attribute,$value_attribute);
 	}
 	
@@ -296,7 +277,6 @@ class XMLUtilities
 							if (strtolower($attrName)==strtolower($key_attribute))
 							{
 								$key=$attrNode->value;
-								//echo "<br/>found key :".$key;
 								break;
 							}
 						}
@@ -321,16 +301,14 @@ class XMLUtilities
 						{
 							$child = $node->childNodes->item($i);
 							if ($child->nodeType==XML_CDATA_SECTION_NODE || $child->nodeType==XML_TEXT_NODE)
-								$value.=htmlspecialchars_decode(trim($child->textContent));//,ENT_NOQUOTES);
+								$value.=htmlspecialchars_decode(trim($child->textContent));
 						}
 					}
-					//echo "<br/>entry loaded :".$key;
 					$output[$key]=$value;
 					return $output;	
 				} //end if match
 				else 
 				{
-					//echo "<br/>looking for child nodes at element ".$current_element_path;
 					for ($i=0, $m=$node->childNodes->length; $i<$m; $i++)
 					{
 						$child = $node->childNodes->item($i);
