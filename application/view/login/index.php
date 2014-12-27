0d94e1395cd3d2d198974e5ffdceb5cdb2a4ab81
Panique <panique@web.de>
2014-12-27 16:21:21 +0100

    frontend text fix

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -15,7 +15,7 @@
                 <input type="text" name="user_name" placeholder="Username (or email)" required />
                 <input type="password" name="user_password" placeholder="Password" required />
                 <input type="checkbox" name="user_rememberme" class="remember-me-checkbox" />
-                <label class="remember-me-label">Keep me logged in (for XXX weeks)</label>
+                <label class="remember-me-label">Remember me for 2 weeks</label>
                 <input type="submit" class="login-submit-button" value="Log in"/>
             </form>
             <div>
