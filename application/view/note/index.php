d60ed11d57ef0859e4c229d110cf1bb80174b6a1
Panique <panique@web.de>
2014-12-28 17:03:11 +0100

    note-index view refactoring

--- application/view/note/index.php
+++ application/view/note/index.php
@@ -1,30 +1,43 @@
-<div class="content">
-    <h1>Create new note</h1>
-    <h3>"Notes" are just an example of how to create, show (read), edit (update) and delete things. CRUD, you know...</h3>
+<div class="container">
+    <h1>NoteController/index</h1>
+    <div class="box">
 
-    <!-- echo out the system feedback (error and success messages) -->
-    <?php $this->renderFeedbackMessages(); ?>
+        <!-- echo out the system feedback (error and success messages) -->
+        <?php $this->renderFeedbackMessages(); ?>
 
-    <form method="post" action="<?php echo URL;?>note/create">
-        <label>Text of new note: </label><input type="text" name="note_text" />
-        <input type="submit" value='Create this note' autocomplete="off" />
-    </form>
+        <h3>What happens here ?</h3>
+        <p>
+            This is just a simple CRUD implementation. Creating, reading, updating and deleting things.
+        </p>
+        <p>
+            <form method="post" action="<?php echo URL;?>note/create">
+                <label>Text of new note: </label><input type="text" name="note_text" />
+                <input type="submit" value='Create this note' autocomplete="off" />
+            </form>
+        </p>
 
-    <h1 style="margin-top: 50px;">List of your notes</h1>
-
-    <table>
-    <?php
-        if ($this->notes) {
-            foreach($this->notes as $key => $value) {
-                echo '<tr>';
-                echo '<td>' . htmlentities($value->note_text) . '</td>';
-                echo '<td><a href="'. URL . 'note/edit/' . $value->note_id.'">Edit</a></td>';
-                echo '<td><a href="'. URL . 'note/delete/' . $value->note_id.'">Delete</a></td>';
-                echo '</tr>';
-            }
-        } else {
-            echo 'No notes yet. Create some !';
-        }
-    ?>
-    </table>
+        <?php if ($this->notes) { ?>
+            <table class="note-table">
+                <thead>
+                <tr>
+                    <td>Id</td>
+                    <td>Note</td>
+                    <td>EDIT</td>
+                    <td>DELETE</td>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($this->notes as $key => $value) { ?>
+                        <tr>
+                            <td><?= htmlentities($value->note_text); ?></td>
+                            <td><a href="<?= URL . 'note/edit/' . $value->note_id; ?>">Edit</a></td>
+                            <td><a href="<?= URL . 'note/delete/' . $value->note_id; ?>">Delete</a></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <?php } else { ?>
+                <div>No notes yet. Create some !</div>
+            <?php } ?>
+    </div>
 </div>
