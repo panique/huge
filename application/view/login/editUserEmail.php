1f4fe9d8d8cd1554fbec9008e04fcc946741ebc0
Panique <panique@web.de>
2014-12-29 22:22:14 +0100

    [git fix commit to override broken case-sensitiveness of git on Windows] filename cleaning, frontend cleaning and spelling fixes

---
+++ application/view/login/editUserEmail.php
@@ -0,0 +1,12 @@
+<div class="content">
+    <h1>Change your email address</h1>
+
+    <!-- echo out the system feedback (error and success messages) -->
+    <?php $this->renderFeedbackMessages(); ?>
+
+    <form action="<?php echo URL; ?>login/editUserEmail_action" method="post">
+        <label>New email address:</label>
+        <input type="text" name="user_email" required />
+        <input type="submit" value="Submit" />
+    </form>
+</div>
