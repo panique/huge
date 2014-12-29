ba2a5b49d4a7b1e655b9f4ca586434df0357e1e8
Panique <panique@web.de>
2014-12-29 18:58:21 +0100

    removed loadModel(), added some comments

--- application/libs/Controller.php
+++ application/libs/Controller.php
@@ -6,15 +6,21 @@
  * 1. initialize a session
  * 2. check if the user is not logged in anymore (session timeout) but has a cookie
  * 3. create a database connection (that will be passed to all models that need a database connection)
- * 4. create a view object
+ * 4. create all the model objects (maybe TODO, definitely TODO for auto-loading)
+ * 5. create a view object
  */
 class Controller
 {
     /** @var object Database The database connection */
     private $database;
 
+    /**
+     * Construct the (base) controller. This happens when a real controller is constructed, like in
+     * the constructor of IndexController when it says: parent::__construct();
+     */
     function __construct()
     {
+        // always initialize a session
         Session::init();
 
         // user is not logged in but has remember-me-cookie ? then try to login with cookie ("remember me" feature)
@@ -25,6 +31,7 @@ class Controller
         }
 
         // create database connection
+        // TODO put the try/catch part inside the Database class ?
         try {
             $this->database = new Database();
         } catch (PDOException $e) {
@@ -39,24 +46,7 @@ class Controller
         $this->LoginModel = new LoginModel($this->database);
         $this->ProfileModel = new ProfileModel($this->database);
 
-        // create a view object (that does nothing, but provides the view render() method)
+        // create a view object to be able to use it inside a controller, like $this->View->render();
         $this->View = new View();
-    }
-
-    /**
-     * loads the model with the given name.
-     * @param $name string name of the model
-     */
-    public function loadModel($name)
-    {
-        $path = PATH_MODEL . ucfirst($name) . 'Model.php';
-
-        if (file_exists($path)) {
-            require PATH_MODEL . ucfirst($name) . 'Model.php';
-            // "Model" has capital letter as this is the second part of the class name, like "LoginModel"
-            $modelName = $name . 'Model';
-            // return the new model object while passing the database connection to the model
-            return new $modelName($this->database);
-        }
     }
 }
