483062f3e65987adc828aaceaf60a77a6937100c
Panique <panique@web.de>
2015-01-01 18:17:06 +0100

    added Request::cookie() getter

--- application/libs/Request.php
+++ application/libs/Request.php
@@ -3,6 +3,9 @@
 /**
  * This is under development. Expect changes!
  * Class Request
+ * Abstracts the access to $_GET, $_POST and $_COOKIE, preventing direct access to these super-globals.
+ * This makes PHP code quality analyzer tools very happy.
+ * @see http://php.net/manual/en/reserved.variables.request.php
  */
 class Request
 {
@@ -27,6 +30,18 @@ class Request
     {
         if (isset($_GET[$key])) {
             return $_GET[$key];
+        }
+    }
+
+    /**
+     * gets/returns the value of a specific key of the COOKIE super-global
+     * @param mixed $key key
+     * @return mixed the key's value or nothing
+     */
+    public static function cookie($key)
+    {
+        if (isset($_COOKIE[$key])) {
+            return $_COOKIE[$key];
         }
     }
 }
