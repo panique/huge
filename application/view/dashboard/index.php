dde749ddc8753e93752abc66e9d4388c26103da0
Panique <panique@web.de>
2015-01-01 21:20:32 +0100

    fe fix

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -1,13 +1,15 @@
-<div class="content">
-    <h1>Dashboard</h1>
+<div class="container">
+    <h1>DashboardController/index</h1>
+    <div class="box">
 
-    <!-- echo out the system feedback (error and success messages) -->
-    <?php $this->renderFeedbackMessages(); ?>
+        <!-- echo out the system feedback (error and success messages) -->
+        <?php $this->renderFeedbackMessages(); ?>
 
-    <h3>This is an area that's only visible for logged in users</h3>
-
-    Try to log out, an go to /dashboard/ again. You'll be redirected to /index/ as you are not logged in.
-    <br/><br/>
-    You can protect a whole section in your app within the according controller (here: controllers/dashboard.php)
-    by placing <span style='font-style: italic;'>Auth::handleLogin();</span> into the constructor.
+        <h3>What happens here ?</h3>
+        <p>
+            This is an area that's only visible for logged in users. Try to log out, an go to /dashboard/ again. You'll
+            be redirected to /index/ as you are not logged in. You can protect a whole section in your app within the
+            according controller by placing <i>Auth::handleLogin();</i> into the constructor.
+        <p>
+    </div>
 </div>
