d7661b84e1c86d20e8afe8031797312872074ce4
Panique <panique@web.de>
2015-01-18 17:43:30 +0100

    avatar upload info message

--- application/view/login/uploadAvatar.php
+++ application/view/login/uploadAvatar.php
@@ -4,6 +4,11 @@
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
 
+    <div class="feedback info">
+        If you still see the old picture after uploading a new one: Hard-Reload the page with F5! Your browser doesn't
+        realize there's a new image as new and old one have the same filename.
+    </div>
+
     <form action="<?php echo URL; ?>login/uploadAvatar_action" method="post" enctype="multipart/form-data">
         <label for="avatar_file">Select an avatar image from your hard-disk (will be scaled to 44x44 px):</label>
         <input type="file" name="avatar_file" required />
