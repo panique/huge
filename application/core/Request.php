01ac28ef8626a6cfea25cc33e1e6711d8753bd95
Panique <panique@web.de>
2015-01-20 23:43:49 +0100

    added optional cleaning of POST vars https://github.com/panique/php-login/issues/550

--- application/core/Request.php
+++ application/core/Request.php
@@ -10,14 +10,20 @@
 class Request
 {
     /**
-     * gets/returns the value of a specific key of the POST super-global
+     * Gets/returns the value of a specific key of the POST super-global.
+     * When using just Request::post('x') it will return the raw and untouched $_POST['x'], when using it like
+     * Request::post('x', true) then it will return a trimmed and stripped $_POST['x'] !
+     *
      * @param mixed $key key
+     * @param bool $clean marker for optional cleaning of the var
      * @return mixed the key's value or nothing
      */
-    public static function post($key)
+    public static function post($key, $clean = false)
     {
         if (isset($_POST[$key])) {
-            return $_POST[$key];
+            // we use the Ternary Operator here which saves the if/else block
+            // @see http://davidwalsh.name/php-shorthand-if-else-ternary-operators
+            return ($clean) ? trim(strip_tags($_POST[$key])) : $_POST[$key];
         }
     }
 
