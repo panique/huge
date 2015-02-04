7fd0fa7a9f2eaaf940c3405a3e8bb05cf3b843a5
Panique <panique@web.de>
2015-02-04 18:12:52 +0100

    frontend styling verification page

--- application/view/login/verify.php
+++ application/view/login/verify.php
@@ -1,8 +1,12 @@
-<div class="content">
+<div class="container">
+
     <h1>Verification</h1>
+    <div class="box">
 
-    <!-- echo out the system feedback (error and success messages) -->
-    <?php $this->renderFeedbackMessages(); ?>
+        <!-- echo out the system feedback (error and success messages) -->
+        <?php $this->renderFeedbackMessages(); ?>
 
-    <a href="<?php echo Config::get('URL'); ?>login/index">Go to login</a>
+        <a href="<?php echo Config::get('URL'); ?>">Go back to home page</a>
+    </div>
+
 </div>
