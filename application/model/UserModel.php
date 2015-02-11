4eb8266613b8eedf6546e779b0de6c7ab43d8bd4
oisian1 <josh_kirkpatrick@hotmail.co.uk>
2015-02-11 10:06:00 +0000

    Added account deleted and susspension

--- application/model/UserModel.php
+++ application/model/UserModel.php
@@ -301,7 +301,7 @@ class UserModel
     {
         $database = DatabaseFactory::getFactory()->getConnection();
 
-        $sql = "SELECT user_id, user_name, user_email, user_password_hash, user_active, user_account_type,
+        $sql = "SELECT user_id, user_name, user_email, user_password_hash, user_active, user_suspension_timestamp, user_account_type,
                        user_failed_logins, user_last_failed_login
                   FROM users
                  WHERE (user_name = :user_name OR user_email = :user_name)
