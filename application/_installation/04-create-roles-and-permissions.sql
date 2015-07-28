5d3df5d5115ef3a1c993460793f5cdc904647ffe
Can <mcanatalay@hotmail.co.uk>
2015-07-28 23:25:27 +0300

    UPDATED SQL
    Fix some syntax error.

--- application/_installation/04-create-roles-and-permissions.sql
+++ application/_installation/04-create-roles-and-permissions.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE IF NOT EXISTS `roles` (
  `role_id` int(3) NOT NULL AUTO_INCREMENT,
- `role_inherit_id` int(3) NOT NULL AUTO_INCREMENT,
+ `role_inherit_id` int(3) NOT NULL DEFAULT '-1',
  `role_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
- PRIMARY KEY (`role_id`),
+ PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `permissions` (
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS `permissions` (
  `role_id` int(3) NOT NULL,
  `permission_name` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
  `permission_granted` tinyint(1) NOT NULL,
- PRIMARY KEY (`role_id`),
+ PRIMARY KEY (`permission_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
