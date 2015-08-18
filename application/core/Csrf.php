2e836fb18cf2be54635f7d6ca761f8bb2db9bf06
Omar El Gabry <omar.elgabry.93@gmail.com>
2015-08-18 04:15:38 +0200

    Update Csrf.php

--- application/core/Csrf.php
+++ application/core/Csrf.php
@@ -29,8 +29,8 @@ class Csrf {
     public static function makeToken() {
 
         $max_time    = 60 * 60 * 24; // token is valid for 1 day
-        $stored_time = Session::get('csrf_token');
-        $csrf_token  = Session::get('csrf_token_time');
+        $stored_time = Session::get('csrf_token_time');
+        $csrf_token  = Session::get('csrf_token');
 
         if($max_time + $stored_time <= time() || empty($csrf_token)){
             Session::set('csrf_token', md5(uniqid(rand(), true)));
