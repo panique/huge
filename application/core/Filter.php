846d0ab0491478e73f7cdd8651b0b3abdee72f94
videsignz <jlc822@yahoo.com>
2016-02-12 20:40:25 -0500

    Recursive XSS Filter
    Edited the XXS Filter to not only accept individual values but also
    arrays and objects. This is useful to use directly after a database
    call.

--- application/core/Filter.php
+++ application/core/Filter.php
@@ -51,9 +51,19 @@ class Filter
     public static function XSSFilter(&$value)
     {
         if (is_string($value)) {
+			
             $value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-        }
+			
+        }elseif(is_array($value) || is_object($value)) {
+			
+			foreach ($value as $key => &$array_value) {
+				
+				self::XSSFilter($array_value);
+				
+			}
+			
+		}
 
         return $value;
-    }
+    }	
 }
